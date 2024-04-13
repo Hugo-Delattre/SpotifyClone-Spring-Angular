a99c1514c81dd5f10304465122b12de7e8ca5de5
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-04-13 12:31:47 +0200

    initial commit

---
+++ spotify-clone-back/src/test/java/dev/hugodelattre/spotifycloneback/SpotifyCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hugodelattre.spotifycloneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCloneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spotify-clone-front/src/app/shared/font-awesome-icon.ts
@@ -0,0 +1,10 @@
+import {faBook, faCirclePlay, faHome, faPlus, faSearch} from "@fortawesome/free-solid-svg-icons";
+import {IconDefinition} from "@fortawesome/free-brands-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+  faCirclePlay,
+  faHome,
+  faSearch,
+  faBook,
+  faPlus
+];
